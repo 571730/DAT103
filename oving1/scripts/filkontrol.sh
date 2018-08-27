e107bd62e24027291dbe7f8317a64dc708774777
571730 <571730@stud.hvl.no>
2018-08-27 03:40:31 -0700

    oppgave 16

--- oving1/scripts/filkontrol.sh
+++ oving1/scripts/filkontrol.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-RUN=true
 OPPRETTET=false
-while [$RUN] do
-	sleep $2
-	if[-e $1 == 0] && [$OPPRETTET != true]
-	then
-		echo "Filen ble opprettet!"
-		$OPPRETTET=true
-	elif [-e $1 -gt 0] && [$OPPRETTET == true]
-	then
-		echo "Filen er blitt slettet"
+FILETIME=0
+while true; do
+   sleep $2
+   if [ -e "$1" ] && [ "$OPPRETTET" != true ]; then
+	echo "Filen $1 ble opprettet!"
+	OPPRETTET=true
+	FILETIME=`stat -c %Y $1`
+	echo "$FILETIME"
+   elif [ ! -e "$1"  ] && [ "$OPPRETTET" == true ]; then
+	echo "Filen $1 er blitt slettet"
+   elif [ -e "$1" ] && [ "$OPPRETTET" == true ]; then
+	if [ "$FILETIME" == `stat -c %Y $1` ]; then
+            echo "$1 eksisterer"
 	else
-		echo "Venter.."
+	    echo "$1 er blitt endret"
+	    FILETIME=`stat -c %Y $1`
 	fi
+   else
+	echo "Venter.."
+   fi
 done
 
 	
