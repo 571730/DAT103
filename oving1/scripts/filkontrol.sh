d568483f1a23c61ed5e8ef1958ba33519001a695
571730 <571730@stud.hvl.no>
2018-09-05 00:16:56 -0700

    Ferdig

--- oving1/scripts/filkontrol.sh
+++ oving1/scripts/filkontrol.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-OPPRETTET=false
+EKSISTERER=false
 FILETIME=0
-while true; do
-   if [ -e "$1" ] && [ "$OPPRETTET" != true ]; then
+RUN=true
+if [ -e "$1" ]; then
+    EKSISTERER=true
+    FILETIME=`stat -c %Y $1`
+fi
+while "$RUN"; do
+   sleep $2
+   if [ -e "$1" ] && [ "$EKSISTERER" != true ]; then
 	echo "Filen $1 ble opprettet!"
-	OPPRETTET=true
-	FILETIME=`stat -c %Y $1`
-   elif [ ! -e "$1"  ] && [ "$OPPRETTET" == true ]; then
+	RUN=false
+   elif [ ! -e "$1"  ] && [ "$EKSISTERER" == true ]; then
 	echo "Filen $1 er blitt slettet"
-   elif [ -e "$1" ] && [ "$OPPRETTET" == true ]; then
+	RUN=false
+   elif [ -e "$1" ] && [ "$EKSISTERER" == true ]; then
 	if [ "$FILETIME" == `stat -c %Y $1` ]; then
             echo "$1 eksisterer"
 	else
 	    echo "$1 er blitt endret"
 	    FILETIME=`stat -c %Y $1`
+	    RUN=false
 	fi
    else
 	echo "Venter.."
    fi
-   sleep $2
 done
 
 	
